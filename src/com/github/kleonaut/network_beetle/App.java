9ddf5aa203fd996dfbde631f3d805f59ce63869a
Ven <70820375+venny-ven@users.noreply.github.com>
2026-02-01 21:10:18 -0600

    Increase log size

--- src/com/github/kleonaut/network_beetle/App.java
+++ src/com/github/kleonaut/network_beetle/App.java
@@ -3,7 +3,7 @@ package com.github.kleonaut.network_beetle;
 public class App
 {
     public static final String NAME = "NetworkBeetle";
-    public static final int MAX_LOG_LENGTH = 200;
+    public static final int MAX_LOG_LENGTH = 800;
     public static final int VERIFICATION_DELAY = 5000;
     public static final int SEARCH_DELAY = 2000;
 
